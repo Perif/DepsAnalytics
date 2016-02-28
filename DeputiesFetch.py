662abd7e4ed17e680ba3030f1f1f2856eafe251c
Pierre-Yves Aquilanti <sperif@gmail.com>
2016-02-28 17:49:02 -0600

    Add output to a conditionally created directory

--- DeputiesFetch.py
+++ DeputiesFetch.py
@@ -1,9 +1,12 @@
 import json
 import urllib2
+import os
 import pickle as pkl
 from progressbar import ProgressBar
 pbar = ProgressBar()
 
+
+output_directory = 'deputy_data'
 
 print "Download deputes en mandat"
 response = urllib2.urlopen('http://www.nosdeputes.fr/deputes/enmandat/json')
@@ -24,14 +27,19 @@ for url in pbar(urls):
   deputes.append(dep_json)
 
 
+# create the output directory if it does not exists
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+
 print "Write to disk"
 # export to pickle
-with open('deputes_data.pkl', 'wb') as output:
+with open(output_directory+'/deputes_data.pkl', 'wb') as output:
   pkl.dump(deputes,output)
-with open('deputes_list.pkl', 'wb') as output:
+with open(output_directory+'/deputes_list.pkl', 'wb') as output:
   pkl.dump(deplist,output)
 # export to json
-with open('deputes_data.json', 'wb') as output:
+with open(output_directory+'/deputes_data.json', 'wb') as output:
   json.dump(deputes,output)
-with open('deputes_list.json', 'wb') as output:
+with open(output_directory+'/deputes_list.json', 'wb') as output:
   json.dump(deplist,output)
